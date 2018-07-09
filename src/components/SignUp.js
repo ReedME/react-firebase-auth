23301a3a21ac3319fa3b3d2fc7d92fee4556161f
Brendan Reed <brendan.james.reed@gmail.com>
2018-07-09 21:09:25 +1000

    structured signup page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () =>
+import * as routes from '../constants/routes';
+
+const SignUpPage = () =>
   <div>
-    <h1>SignUp</h1>
+    <h1>Sign Up</h1>
+    <SignUpForm />
   </div>
 
-export default SignUp;
+class SignUpForm extends Component {
+  constructor(props){
+    super(props);
+  }
+  onSubmit = (event) => {
+
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.onSubmit}>
+
+      </form>
+    );
+  }
+}
+
+const SignUpLink = () =>
+<p>
+Don't have an account?
+{' '}
+<Link to={routes.SIGN_UP}> Sign Up</Link>
+</p>
+
+export default SignUpPage;
+
+export {
+  SignUpForm,
+  SignUpLink,
+}
