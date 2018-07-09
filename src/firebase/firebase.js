337cd2c23b5dfd4d2f593d9cd1ce4421a838d7c1
Brendan Reed <brendan.james.reed@gmail.com>
2018-07-09 20:54:27 +1000

    setup firebase config and api interface

---
+++ src/firebase/firebase.js
@@ -0,0 +1,37 @@
+/*
+firebase.js: The file where all the configuration goes that you 
+have seen previously on your Firebase dashboard. In addition, 
+Firebase itself will be instantiated in this file.
+
+Can run two projects on firebase one for production one for live
+would just need two configs and initialise based on NODE_ENV
+*/
+
+import firebase from 'firebase/app';
+
+import * as keys from '../constants/firebaseKeys';
+
+// import our project keys and assign them
+
+const config = {
+  apiKey: keys.API_KEY,
+  authDomain: keys.AUTH_DOMAIN,
+  databaseURL: keys.DATABASE_URL,
+  projectId: keys.PROJECT_ID,
+  storageBucket: keys.STORAGE_BUCKET,
+  messagingSenderId: keys.MESSAGING_SENDER_ID,
+}
+
+// initialise firebase if it isn't already initialised.
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+// initialise auth object from firebase
+
+const auth = firebase.auth();
+
+export {
+    auth,
+};
